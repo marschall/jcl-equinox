7600382bdda8c06b04e977c988077507a5fa9b46
Philippe Marschall <philippe.marschall@gmail.com>
2023-07-14 13:22:14 +0200

    small cleanup

--- src/com/github/marschall/jclequinox/EquinoxLogFactory.java
+++ src/com/github/marschall/jclequinox/EquinoxLogFactory.java
@@ -7,7 +7,6 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogConfigurationException;
 import org.apache.commons.logging.LogFactory;
 import org.eclipse.equinox.log.ExtendedLogService;
 import org.eclipse.equinox.log.Logger;
@@ -67,7 +66,7 @@ public class EquinoxLogFactory extends LogFactory {
   }
 
   @Override
-  public Log getInstance(Class clazz) throws LogConfigurationException {
+  public Log getInstance(Class clazz) {
     String className = clazz.getName();
     Log slf4jLogger = this.loggerMap.get(className);
     if (slf4jLogger == null) {
@@ -86,7 +85,7 @@ public class EquinoxLogFactory extends LogFactory {
   }
 
   @Override
-  public Log getInstance(String name) throws LogConfigurationException {
+  public Log getInstance(String name) {
     Log slf4jLogger = this.loggerMap.get(name);
     if (slf4jLogger == null) {
       Logger equinoxLogger = this.logService.getLogger(name);
